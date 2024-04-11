bd85c76be9172145e45efd77ce66310a449b675a
KawaiKenta <kawai@iseyama.online>
2024-04-11 08:40:29 +0000

    Add feature for connect MetaMask

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
 	import '../app.css';
+	import favicon from '$lib/assets/favicon.png';
 </script>
 
+<svelte:head>
+	<meta charset="utf-8" />
+	<link rel="shortcut icon" href={favicon} type="image/x-icon" />
+</svelte:head>
 <slot />
+
+<style lang="postcss">
+	:global(body) {
+		@apply bg-gray-900;
+		@apply text-white;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1 class="p-4 pt-2 text-3xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.png';
+
+	const connectToMetaMask = async () => {
+		const provider = window.ethereum;
+		if (provider) {
+			try {
+				await provider.request({ method: 'eth_requestAccounts' });
+				console.log('Connected to MetaMask');
+			} catch (error) {
+				throw new Error('Error connecting to MetaMask');
+			}
+		} else {
+			throw new Error('MetaMask not found');
+		}
+	};
+</script>
+
+<svelte:head>
+	<title>IoT Data Market Place</title>
+</svelte:head>
+
+<header class="px-4 py-2 shadow-sm shadow-indigo-500/50">
+	<div class="flex items-center justify-between">
+		<a href="/" class="flex items-center">
+			<img src={favicon} alt="" class="h-8 w-8" />
+			<h1 class="ml-3 text-xl">IoT Marketplace</h1>
+		</a>
+		<div id="icons" class="">
+			<button
+				on:click={() => {
+					connectToMetaMask().catch((error) => {
+						alert(`Error: ${error.message}\nPlease install MetaMask and try again.`);
+					});
+				}}
+				class="rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 px-4 py-2 text-white shadow-lg hover:bg-gradient-to-bl"
+			>
+				Connect your wallet!</button
+			>
+		</div>
+	</div>
+</header>
+<section id="hero"></section>
+<main>
+	<section id="merchandises"></section>
+</main>
