e3dfa74c5697257f7628c30660345fdff2e30414
KawaiKenta <kawai@iseyama.online>
2024-04-16 02:48:01 +0000

    Add: カードコンポーネントの作成

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.png';
+	import Card from '$lib/components/Card.svelte';
+	import { ethers } from 'ethers';
+
+	let provider: ethers.BrowserProvider | undefined = $state();
+	let signer: ethers.JsonRpcSigner | undefined = $state();
 
 	const connectToMetaMask = async () => {
-		const provider = window.ethereum;
-		if (provider) {
-			try {
-				await provider.request({ method: 'eth_requestAccounts' });
-				console.log('Connected to MetaMask');
-			} catch (error) {
-				throw new Error('Error connecting to MetaMask');
-			}
-		} else {
+		const windowProvider = window.ethereum;
+		if (windowProvider == null) {
 			throw new Error('MetaMask not found');
+		}
+		try {
+			const provider = await new ethers.BrowserProvider(windowProvider);
+			const signer = await provider.getSigner();
+			return { provider, signer };
+		} catch (error) {
+			throw new Error('Error connecting to MetaMask');
 		}
 	};
 </script>
@@ -26,12 +31,20 @@
 			<img src={favicon} alt="" class="h-8 w-8" />
 			<h1 class="ml-3 text-xl">IoT Marketplace</h1>
 		</a>
-		<div id="icons" class="">
+		<div id="icons" class="flex items-center">
+			{#if signer}
+				<p class="mr-2">Welcome! <span class="text-blue-500">{signer.address}</span></p>
+			{/if}
 			<button
 				on:click={() => {
-					connectToMetaMask().catch((error) => {
-						alert(`Error: ${error.message}\nPlease install MetaMask and try again.`);
-					});
+					connectToMetaMask()
+						.then((res) => {
+							provider = res.provider;
+							signer = res.signer;
+						})
+						.catch((error) => {
+							alert(`Error: ${error.message}\nPlease install MetaMask and try again.`);
+						});
 				}}
 				class="rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 px-4 py-2 text-white shadow-lg hover:bg-gradient-to-bl"
 			>
@@ -41,6 +54,11 @@
 	</div>
 </header>
 <section id="hero"></section>
-<main>
-	<section id="merchandises"></section>
-</main>
+
+<section id="merchandises" class="container mx-auto mt-4">
+	<div class="flex flex-wrap">
+		<div class="w-full md:w-1/2 lg:w-1/3">
+			<Card title="aaaa" content="drer"></Card>
+		</div>
+	</div>
+</section>
